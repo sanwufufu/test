b3d99b8b778d1a170240443715586d8300e7880f
sanwu <apprilfuu@163.com>
2019-12-18 13:02:19 +0800

    feature

--- target/test/cucumber/report.js
+++ target/test/cucumber/report.js
@@ -1,6 +1,58 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("classpath:hellocucumber/feature/test.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("classpath:hellocucumber/feature/test1.feature");
 formatter.feature({
-  "name": "test",
+  "name": "test1",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "name": "Friday",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@simpleDemo"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "today is Sunday",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "Stepdefs.today_is_Sunday()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I ask whether it\u0027s Friday yet",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "Stepdefs.i_ask_whether_it_s_Friday_yet()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I should be told \"Nope\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Stepdefs.i_should_be_told(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.uri("classpath:hellocucumber/feature/test333.feature");
+formatter.feature({
+  "name": "test333",
   "description": "  Everybody wants to know when it\u0027s",
   "keyword": "Feature"
 });
@@ -153,58 +205,6 @@ formatter.scenario({
       "name": "@simpleDemo"
     }
   ]
-});
-formatter.step({
-  "name": "today is Sunday",
-  "keyword": "Given "
-});
-formatter.match({
-  "location": "Stepdefs.today_is_Sunday()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "I ask whether it\u0027s Friday yet",
-  "keyword": "When "
-});
-formatter.match({
-  "location": "Stepdefs.i_ask_whether_it_s_Friday_yet()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "I should be told \"Nope\"",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "Stepdefs.i_should_be_told(String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.after({
-  "status": "passed"
-});
-formatter.uri("classpath:hellocucumber/feature/test1.feature");
-formatter.feature({
-  "name": "test1",
-  "description": "",
-  "keyword": "Feature"
-});
-formatter.scenario({
-  "name": "Friday",
-  "description": "",
-  "keyword": "Scenario",
-  "tags": [
-    {
-      "name": "@simpleDemo"
-    }
-  ]
-});
-formatter.before({
-  "status": "passed"
 });
 formatter.step({
   "name": "today is Sunday",
